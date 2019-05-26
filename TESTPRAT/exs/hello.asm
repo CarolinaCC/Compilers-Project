33ed391d725c57fceb5d147826ea06e6b801ea8b
Carolina Maria Da Cunha Carreira <ist187641@sigma00.ist.utl.pt>
2019-05-26 22:12:28 +0100

    d

---
+++ TESTPRAT/exs/hello.asm
@@ -0,0 +1,105 @@
+; TEXT
+segment	.text
+; ALIGN
+align	4
+; GLOBL
+global	$_entry:function
+; LABEL
+$_entry:
+; ENTER
+	push	ebp
+	mov	ebp, esp
+	sub	esp, 8
+; RODATA
+segment	.rodata
+; ALIGN
+align	4
+; LABEL
+$_i1:
+; CHAR
+	db	0x6F
+; CHAR
+	db	0x6C
+; CHAR
+	db	0x61
+; CHAR
+	db	0x0A
+; CHAR
+	db	0x00
+; TEXT
+segment	.text
+; ADDR
+	push	dword $_i1
+; COPY
+	push	dword [esp]
+; LOCAL
+	lea	eax, [ebp+-8]
+	push	eax
+; STORE
+	pop	ecx
+	pop	eax
+	mov	[ecx], eax
+; TRASH
+	add	esp, 4
+; LOCAL
+	lea	eax, [ebp+-8]
+	push	eax
+; LOAD
+	pop	eax
+	push	dword [eax]
+; IMM
+	push	dword 0
+; IMM
+	push	dword 1
+; MUL
+	pop	eax
+	imul	dword eax, [esp]
+	mov	[esp], eax
+; ADD
+	pop	eax
+	add	dword [esp], eax
+; LDCHR
+	pop	ecx
+	movsx	eax,byte [ecx]
+	push	eax
+; CALL
+	call	$_printi
+; TRASH
+	add	esp, 4
+; PUSH
+	push	eax
+; TRASH
+	add	esp, 4
+; IMM
+	push	dword 0
+; COPY
+	push	dword [esp]
+; LOCAL
+	lea	eax, [ebp+-4]
+	push	eax
+; STORE
+	pop	ecx
+	pop	eax
+	mov	[ecx], eax
+; TRASH
+	add	esp, 4
+; LOCAL
+	lea	eax, [ebp+-4]
+	push	eax
+; LOAD
+	pop	eax
+	push	dword [eax]
+; POP
+	pop	eax
+; LEAVE
+	leave
+; RET
+	ret
+; EXTRN
+extern	$_prints
+; EXTRN
+extern	$_printi
+; EXTRN
+extern	$_factorial
+; EXTRN
+extern	$_strcmp
